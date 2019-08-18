d4656c4363f99c4340e44a2a9dc2ffd88e81f075
James Bowling <jbowling@vmware.com>
2019-08-18 11:56:35 -0500

    Fixed missing dependencies

--- scripts/setRepos.sh
+++ scripts/setRepos.sh
@@ -1,4 +1,5 @@
 echo "Configuring repos and updating package index" \
+    && apt-get update && apt-get install -y curl gnupg \
     && echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list \
     && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - \
     && curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null \
@@ -8,5 +9,5 @@ echo "Configuring repos and updating package index" \
     && echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list \
     && wget -q -O - https://raw.githubusercontent.com/starkandwayne/homebrew-cf/master/public.key | apt-key add - \
     && echo "deb http://apt.starkandwayne.com stable main" | tee /etc/apt/sources.list.d/starkandwayne.list \
-    && apt-get update && apt-get install -y apt-transport-https build-essential curl dnsutils git iputils-ping lsb-release mtr net-tools openssh-server \
+    && apt-get update && apt-get install -y apt-transport-https build-essential dnsutils git iputils-ping lsb-release mtr net-tools openssh-server \
     openssl python3 python3-dev python3-pip python3-setuptools ruby ruby-dev traceroute vim wget
