e058491d9cd79f30cefa3c163b1b2d4507eba27f
James Bowling <jbowling@vmware.com>
2019-08-18 12:18:19 -0500

    Fixed missing slash

--- scripts/setRepos.sh
+++ scripts/setRepos.sh
@@ -1,6 +1,6 @@
 echo "Configuring repos and updating package index" \
     && apt-get update && apt-get install -y apt-transport-https build-essential curl dnsutils git gnupg iputils-ping lsb-release mtr net-tools openssh-server \
-    openssl python3 python3-dev python3-pip python3-setuptools ruby ruby-dev traceroute vim wget
+    openssl python3 python3-dev python3-pip python3-setuptools ruby ruby-dev traceroute vim wget \
     && echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list \
     && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - \
     && curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null \
