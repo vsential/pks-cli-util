1e461eb8117add0ad9958423a9436d3788b63842
James Bowling <james.bowling@vsential.com>
2019-08-20 20:07:26 -0500

    merge utility updates (#9)
    * added kubectl completion
    
    * updated TODO
    
    * fixed alias build error
    
    * updated pks cli to 1.5.0

--- scripts/installPKScli.sh
+++ scripts/installPKScli.sh
@@ -1,7 +1,7 @@
 # Install PKS CLI
 echo "Installing PKS CLI" \
-    && wget -q --no-check-certificate https://www.dropbox.com/s/ok6g2mxkiycudby/pks-linux-amd64-1.4.0-build.230 \
-    && mv pks-linux-amd64-1.4.0-build.230 /usr/local/bin/pks \
+    && wget -q --no-check-certificate https://www.dropbox.com/s/4zm1216a89y5j78/pks-linux-amd64-1.5.0-build.291 \
+    && mv pks-linux-amd64-1.5.0-build.291 /usr/local/bin/pks \
     && chmod +x ./usr/local/bin/pks \
     && which pks \
     && pks --version
