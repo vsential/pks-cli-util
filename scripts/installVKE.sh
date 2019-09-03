7978694b74d5e9061ea0eceeb11dd28b947b0a44
James Bowling <jbowling@vmware.com>
2019-09-03 16:40:42 -0500

    removed extra periods

--- scripts/installVKE.sh
+++ scripts/installVKE.sh
@@ -2,4 +2,4 @@
 echo "Installing VKE" \
     && wget -q https://s3.amazonaws.com/vke-cli-us-east-1/latest/linux64/vke \
     && chmod +x ./vke \
-    && mv ./vke /usr/local/bin
+    && mv vke /usr/local/bin
